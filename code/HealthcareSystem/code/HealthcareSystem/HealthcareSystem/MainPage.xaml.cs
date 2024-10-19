75bc6ee0dd44883fc69a8bc9a5e55ec3b3df56d5
Trinidad Dena <tdena1@my.westga.edu>
2024-10-19 14:16:59 -0400

    Added views for Login, Admin, Nurse
    created blank page with codebehind for the user stories that involve logging in for the entry into the system, editing and adding a patient for the nurse role, and adding a nurse into the system to have an account for the admin role

--- code/HealthcareSystem/code/HealthcareSystem/HealthcareSystem/MainPage.xaml.cs
+++ code/HealthcareSystem/code/HealthcareSystem/HealthcareSystem/MainPage.xaml.cs
@@ -13,18 +13,29 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace HealthcareSystem
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            LoadUserInfo();
+        }
+
+        private void LoadUserInfo()
+        {
+            UserInfo.Text = "Logged in as: Nurse Jane Doe (jane.doe)";
+        }
+
+        private void ManagePatients_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(PatientManagementPage));
+        }
+
+        private void AdminFunctions_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AdminPage));
         }
     }
 }
