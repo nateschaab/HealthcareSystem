3d79eb24060108e54e40434b33053253561669e0
Trinidad Mario Dena <tdena1@my.westga.edu>
2024-10-19 23:58:43 -0400

    Added base database functionality
    Added starter files from in-class exercise 10
    Issues with C# version
    Using statement does not seem compatible
    table.asEnumerable does not exist in version either
    C# version for project seems to be 7.3 and we need 8.0 for some features
    May just need to close connections explicitly without using statement
    Research indicates UWP latest target version for Windows 10 does not seem to support these features/C# version
    UWP language seems to be dependent on target platform

---
+++ code/HealthcareSystem/HealthcareSystem/Model/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DBAccess.Model
+{
+/// <summary>
+/// The Employee Class.
+/// </summary>
+   public class Employee
+    {
+        public string FirstName { get; }
+        public int?  DepartmentNumber { get;  }
+        public DateTime BirthDate { get;  }
+
+        public Employee(string firstname, DateTime? birthdate, int? dno)
+        {
+            this.FirstName = firstname ?? throw new ArgumentNullException(nameof(firstname), $"{nameof(firstname)} cannot be null");
+            this.BirthDate = birthdate ?? throw new ArgumentNullException(nameof(birthdate), $"{nameof(birthdate)} cannot be null");
+
+
+            if (dno < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dno), $"{nameof(dno)} cannot be negative");
+            }
+
+            this.DepartmentNumber = dno;
+        }
+    }
+}
